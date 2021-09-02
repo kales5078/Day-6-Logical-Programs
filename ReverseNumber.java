f22c398e50bf0a6b76fd1d1bfb3a109e76f87cec
kales5078 <88039478+kales5078@users.noreply.github.com>
2021-09-02 21:02:43 +0530

    Add : Initial Commit

---
+++ ReverseNumber.java
@@ -0,0 +1,29 @@
+package logicalprogram;
+import java.util.*;
+public class ReverseNumber {
+	
+	  // method created for to get reverse number
+	public static  void revNum(int number) {
+		int reminder , reverse = 0;
+		
+		while(number != 0)
+		{
+			// get reverse number
+			reminder= number % 10;
+			reverse= reverse * 10 + reminder;
+			number = number / 10 ;
+		}		
+			
+			System.out.println("The Reverse of given number is " + reverse );
+	}
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		   int num = sc.nextInt();
+		   revNum(num);
+
+	}
+
+}
